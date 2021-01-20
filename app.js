02c5ea8271e16d998361b912dd7ace1221ac9a95
tomaszduda1990 <tomasz.duda.krk@gmail.com>
2021-01-20 20:28:04 +0100

    added express to project

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const http = require("http");
-const requestHandler = require("./routes");
-const server = http.createServer(requestHandler);
+const express = require("express");
 
-server.listen(3000);
+const app = express();
+app.use("/lorem", (req, res, next) => {
+  console.log("from lorem");
+  res.send("<p>Lorem ipsum</p>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("main page");
+  res.send("<h2>main page</h2>");
+});
+
+app.listen(3000);
--- assignment1/app.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const reqHandler = require("./routes");
-
-const server = http.createServer(reqHandler);
-
-server.listen(3000);
