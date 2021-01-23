d22256393e740fcd57427dd85ff49af71b306f6b
tomaszduda1990 <tomasz.duda.krk@gmail.com>
2021-01-23 17:02:40 +0100

    express practice

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+const path = require("path");
+
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use("/lorem", (req, res, next) => {
-  console.log("from lorem");
-  res.send("<p>Lorem ipsum</p>");
-});
+app.use(express.static(path.join(__dirname, "public")));
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/", (req, res, next) => {
-  console.log("main page");
-  res.send("<h2>main page</h2>");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
